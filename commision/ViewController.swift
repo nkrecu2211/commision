f21aedbecb9a52ea2fe654b4008a32d6721d7fa3
Nikolas Krecu <nkrecu2211@jh-mba158-07.d214.org>
2018-10-05 11:13:07 -0500

    Initial Commit

---
+++ commision/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  commision
+//
+//  Created by Nikolas Krecu on 10/5/18.
+//  Copyright © 2018 John Hersey Highschool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
